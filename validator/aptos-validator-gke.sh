cc3f0c39adf7a6fd790dd14ac2d242a1bef71047
qyeah33 <99518712+qyeah98@users.noreply.github.com>
2022-05-17 15:32:37 +0900

    Update aptos-validator-gke.sh

--- validator/aptos-validator-gke.sh
+++ validator/aptos-validator-gke.sh
@@ -12,73 +12,76 @@ echo "                                      ";
 echo -e "\e[0m"
 echo "=================================================="
 
-sleep 2
+sleep 1
 
 echo -e "\e[1m\e[32m1. Set Validator Parameter \e[0m" && sleep 1
 
-echo -e "\e[1m\e[32m1.1 Set Validator Name \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1.1 Set Validator Name \e[0m"
 read -p "INPUT Validator Name: " VALIDATOR_NAME
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m1.2 Set USERNAME \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1.2 Set USERNAME \e[0m"
 read -p "INPUT Username: " USERNAME
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m1.3 Set Project ID \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1.3 Set Project ID \e[0m"
 read -p "INPUT Project ID: " PROJECT_ID
 
 echo "=================================================="
-echo -e "\e[1m\e[32m1.4 Set Google Cloud Region \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1.4 Set Google Cloud Region \e[0m"
 read -p "INPUT Region: " REGION
 
 echo "=================================================="
-echo -e "\e[1m\e[32m1.5 Set Google Cloud Zone \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1.5 Set Google Cloud Zone \e[0m"
 read -p "INPUT Zone: " ZONE
 
 echo "=================================================="
-echo -e "\e[1m\e[32m1.6 Set Google Cloud Storage \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1.6 Set Google Cloud Storage \e[0m"
 read -p "INPUT Bucket Name: " BUCKET
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m1.7 Set Workspace Name \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1.7 Set Workspace Name \e[0m"
 read -p "INPUT Workspace Name: " WORKSPACE
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m1. Updating list of dependencies... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m2. Updating list of dependencies... \e[0m" && sleep 1
 
+echo -e "\e[1m\e[32m2.1 Set project $PROJECT_ID \e[0m"
 gcloud config set project $PROJECT_ID
 
 if ! which cargo &> /dev/null
 then
+    echo -e "\e[1m\e[32m2.2 Installing rust... \e[0m"
     apt-get update
     curl https://sh.rustup.rs -sSf | sh
-    source $HOME/.cargo/env
+    source $HOME/.cargo/bin
 fi
 
 if ! which aptos &> /dev/null
 then
+    echo -e "\e[1m\e[32m2.3 Installing aptos cli... \e[0m"
     apt install clang
     cargo install --git https://github.com/aptos-labs/aptos-core.git aptos --tag aptos-cli-latest
 fi
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m2. Creating Terraform configuration... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m3. Creating Terraform configuration... \e[0m" && sleep 1
 
-echo -e "\e[1m\e[32m2.1 Creating Workspace... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m3.1 Creating Workspace... \e[0m" && sleep 1
 
 rm -rf ~/$WORKSPACE
 mkdir -p ~/$WORKSPACE
 
-echo -e "\e[1m\e[32m2.2 Creating Google Cloud Storage... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m3.2 Creating Google Cloud Storage... \e[0m" && sleep 1
 
 gsutil mb gs://$BUCKET
 
-echo -e "\e[1m\e[32m2.2 Creating Terafform main.tf... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m3.2 Creating Terafform main.tf... \e[0m" && sleep 1
 
 cd ~/$WORKSPACE
 
@@ -105,7 +108,7 @@ EOF
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m3. Terraform init and apply... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m4. Terraform init and apply... \e[0m" && sleep 1
 
 terraform init
 
@@ -114,7 +117,7 @@ terraform apply
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m4. Getting address... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m5. Getting address... \e[0m" && sleep 1
 
 gcloud container clusters get-credentials aptos-$WORKSPACE --zone=$REGION-$ZONE --project=$PROJECT_ID
 
@@ -123,17 +126,17 @@ export FULLNODE_ADDRESS="$(kubectl get svc ${WORKSPACE}-aptos-node-fullnode-lb -
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m5. Creating Aptos Validator config files... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m6. Creating Aptos Validator config files... \e[0m" && sleep 1
 
-echo -e "\e[1m\e[32m5.1 Generate key pairs... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m6.1 Generate key pairs... \e[0m" && sleep 1
 
 aptos genesis generate-keys --output-dir ~/$WORKSPACE
 
-echo -e "\e[1m\e[32m5.2 Generate validator/fullnode config file... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m6.2 Generate validator/fullnode config file... \e[0m" && sleep 1
 
 aptos genesis set-validator-configuration --keys-dir ~/$WORKSPACE --local-repository-dir ~/$WORKSPACE --username $USERNAME --validator-host $VALIDATOR_ADDRESS:6180 --full-node-host $FULLNODE_ADDRESS:6182
 
-echo -e "\e[1m\e[32m5.3 Creating YAML file... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m6.3 Creating YAML file... \e[0m" && sleep 1
 
 cat <<EOF > layout.yaml
 ---
@@ -143,16 +146,16 @@ users:
 chain_id: 23
 EOF
 
-echo -e "\e[1m\e[32m5.4 Downloading AptosFramework Move bytecode... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m6.4 Downloading AptosFramework Move bytecode... \e[0m" && sleep 1
 wget https://github.com/aptos-labs/aptos-core/releases/download/aptos-framework-v0.1.0/framework.zip
 unzip framework.zip
 
-echo -e "\e[1m\e[32m5.5 Compiling genesis blob and waypoint.... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m6.5 Compiling genesis blob and waypoint.... \e[0m" && sleep 1
 aptos genesis generate-genesis --local-repository-dir ~/$WORKSPACE --output-dir ~/$WORKSPACE
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m6. Inserting genesis.blob, waypoint.txt and identity files... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m7. Inserting genesis.blob, waypoint.txt and identity files... \e[0m" && sleep 1
 kubectl create secret generic ${WORKSPACE}-aptos-node-genesis-e1 \
     --from-file=genesis.blob=genesis.blob \
     --from-file=waypoint.txt=waypoint.txt \
@@ -161,12 +164,15 @@ kubectl create secret generic ${WORKSPACE}-aptos-node-genesis-e1 \
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m7. Aptos Validator Started \e[0m" && sleep 1
+echo -e "\e[1m\e[32m8. Aptos Validator Info \e[0m" && sleep 1
+echo -e "\e[1m\e[39m"    $(cat ~/$WORKSPACE/$USERNAME.yaml)" \n \e[0m"
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32Just wait... " && sleep 30
+
+echo -e "\e[1m\e[32m8. Aptos Validator Started \e[0m" && sleep 1
 kubectl get pods
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32m8. Aptos Validator Info \e[0m" && sleep 1
-echo -e "\e[1m\e[39m"    cat ~/$WORKSPACE/$USERNAME.yaml" \n \e[0m"
-
-echo "=================================================="
